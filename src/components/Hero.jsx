c38a9f62aaa094e4aad4896e9bb0d284fc75b2de
gaohuangan <gaohuangan@yeah.net>
2025-07-30 23:49:03 +0900

    feat: the three pages of the HomePage have been completed v1_20250730_2348

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,6 +28,9 @@ const Hero = () => {
         <button className='bg-white px-10 py-3 rounded-lg boder border-gray-300 hover:scale-102 
         active:scale-95 transition cursor-pointer'>Watch a quick demo</button>
       </div>
+      <div className='flex items-center gap-4 mt-8 mx-auto text-gray-600'>
+        <img src={assets.user_group} alt="" className='h-8' />Trusted by 10k+ people
+      </div>
     </div>
   )
 }
